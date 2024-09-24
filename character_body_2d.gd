9546988f326e38cb6dd48abd28fab6c8e2d8e5e3
Luixz <142626537+LluixZ@users.noreply.github.com>
2024-09-23 21:58:12 -0300

    Update character_body_2d.gd
    Added double jump

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
+var jumpCount = 0
 
 @onready var animador := $anim as AnimatedSprite2D
 
@@ -16,9 +17,15 @@ func _physics_process(delta: float) -> void:
 			animador.play("jump")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if is_on_floor():
+		jumpCount = 0
+	if jumpCount < 1:
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_pressed("ui_accept") and not is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jumpCount +=1
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
